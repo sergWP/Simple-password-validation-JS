dfdd78d8dc5147f850835fd2e9cbcc967f28c528
sergWP <kuzmenko.sergei@gmail.com>
2019-11-10 21:38:24 +0200

    String Modify Section

--- common.js
+++ common.js
@@ -3,7 +3,6 @@ function formValidate() {
 	var getUserPass = document.getElementsByClassName('checkPasswordForm_password')[0].value;
 	var specialChars = new RegExp(/[\[\]?*+|{}\\()@.\n\r]/);
 	var specialNumbers = new RegExp('[0-9]');
-	var result = document.getElementsByClassName('result');
 
 	if (getUserName && getUserPass) {
 		var userName = getUserName;
@@ -23,7 +22,7 @@ function formValidate() {
 				break;
 			default:
 				console.log('Form submited!!!');
-				document.getElementsByClassName('result')[0].innerHTML = 'Form submited!!!';
+				document.getElementsByClassName('formResult')[0].innerHTML = 'Form submited!!!';
 				break;
 		}
 
@@ -31,3 +30,19 @@ function formValidate() {
 		alert('Please, enter your "Name and Password"');
 	}
 }
+
+function stringModify() {
+	var string = '';
+	var symbols = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	var symbolsLength = symbols.length;
+	for ( var i=1; i<=symbolsLength; i++ ) {
+		string += symbols.charAt(Math.floor(Math.random() * symbolsLength));
+	}
+	document.getElementsByClassName('string')[0].innerHTML = string;
+
+	var newString = string.replace(/\s/g, ''); // with regex
+	// var newString = string.replace(' ',''); // work too
+
+	document.getElementsByClassName('stringModify')[0].innerHTML = newString;
+}
+stringModify();
